66ff69c638317cca69031097abb20b575920cd30
Branton Davis <bdavis@innography.com>
2013-10-06 04:19:29 -0500

    Cleaning up setup script and checking for files before building things (for reload).

--- vm/setupEspruinoBuild.sh
+++ vm/setupEspruinoBuild.sh
@@ -1,30 +1,48 @@
 cd /home/vagrant
 
 # run vagrant post install
-#echo "postinstall"
-#if [ -f postinstall.sh ];
-#then
-#  ./postinstall.sh
-#  rm postinstall.sh
-#fi
+if [ -f /home/vagrant/postinstall.sh ];
+then
+  ./postinstall.sh
+  rm postinstall.sh
+fi
 
-echo "git config"
-# TODO: there's probably a better way to handle this
+# TODO: there's probably a better way to handle this, but this gets around warnings
 git config --global user.email "no@no.com"
 git config --global user.name "EspruinoBuildEnvironment"
 
 # install stlink
-git clone https://github.com/texane/stlink.git
-cd stlink
-sudo ./autogen.sh
-./configure
-make
-cd ../
-chown vagrant:vagrant -R stlink
+if [ ! -f /home/vagrant/stlink ];
+then
+  # get stlink source
+  git clone https://github.com/texane/stlink.git
+  cd stlink
 
-# get ARM EABI Toolchain Builder
-#git clone https://github.com/jsnyder/arm-eabi-toolchain.git
+  # build stlink
+  ./autogen.sh
+  ./configure
+  make
+  cd ../
 
-# make!
-#cd arm-eabi-toolchain
-#PROCS=2 make install-cross
+  # fix permissions
+  chown vagrant:vagrant -R stlink
+fi
+
+if [ ! -f /home/vagrant/arm-eabi-toolchain ];
+then
+  # get ARM EABI Toolchain Builder
+  git clone https://github.com/jsnyder/arm-eabi-toolchain.git
+
+  chown vagrant:vagrant -R arm-eabi-toolchain
+
+  # TODO: find out why blueprint misses this, or just add to chef config manually
+  apt-get install texinfo
+
+  # make!
+  cd arm-eabi-toolchain
+  PROCS=2 make install-cross
+
+  # add tools path
+  cd ../
+  echo "PATH=/home/vagrant/arm-cs-tools/bin:$PATH" > /home/vagrant/.profile
+fi
